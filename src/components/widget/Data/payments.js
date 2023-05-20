bb7a9631b64aed16109e4eb6a33cf27b4d55c4e7
usman-finehome <133855254+usman-finehome@users.noreply.github.com>
2023-05-20 16:53:31 +0400

    Modified the payments

--- src/components/widget/Data/payments.js
+++ src/components/widget/Data/payments.js
@@ -7,126 +7,126 @@ const data = [
   {
     companyName: 'Aldar',
     orderNo: '1100',
-    date: ' 12/30',
-    amount: '200',
-    payment_method: 'card',
-    payment_plan: '280',
+    date: '3-05-2023',
+    amount: '7300',
+    payment_method: 'Visa Card',
+    payment_plan: 'Monthly',
     status: 'Paid Subscription'
   },
   {
     companyName: 'Aldar',
-    orderNo: '1100',
-    date: ' 12/30',
+    orderNo: '300',
+    date: '3-05-2023',
+    amount: '2000',
+    payment_method: 'Debit Card',
+    payment_plan: 'Monthly',
+    status: 'Paid Subscription'
+  },
+  {
+    companyName: 'Naruto',
+    orderNo: '1050',
+    date: '02-30-2023',
+    amount: '56000',
+    payment_method: 'Credit Card',
+    payment_plan: 'Monthly',
+    status: 'Paid Subscription'
+  },
+  {
+    companyName: 'Naruto',
+    orderNo: '1400',
+    date: '02-30-2023',
+    amount: '3120',
+    payment_method: 'Cash',
+    payment_plan: 'Yearly',
+    status: 'Paid Subscription'
+  },
+  {
+    companyName: 'Asgurd',
+    orderNo: '1500',
+    date: '02-30-2023',
+    amount: '96501',
+    payment_method: 'Debit Card',
+    payment_plan: 'Yearly',
+    status: 'Paid Subscription'
+  },
+  {
+    companyName: 'Asgurd',
+    orderNo: '1600',
+    date: '02-30-2023',
+    amount: '3256',
+    payment_method: 'Debit Card',
+    payment_plan: 'Monthly',
+    status: 'Paid Subscription'
+  },
+  {
+    companyName: 'Sasuke',
+    orderNo: '1700',
+    date: '12-30-2023',
+    amount: '2030',
+    payment_method: 'Credit Card',
+    payment_plan: 'Monthly',
+    status: 'Paid Subscription'
+  },
+  {
+    companyName: 'Sasuke',
+    orderNo: '1800',
+    date: '12-30-2023',
+    amount: '500',
+    payment_method: 'Visa Card',
+    payment_plan: 'Monthly',
+    status: 'Paid Subscription'
+  },
+  {
+    companyName: 'Broker Company',
+    orderNo: '1900',
+    date: '12-30-2023',
+    amount: '1200',
+    payment_method: 'Credit Card',
+    payment_plan: 'Yearly',
+    status: 'Paid Subscription'
+  },
+  {
+    companyName: 'Managment Company',
+    orderNo: '1101',
+    date: '9-30-2023',
     amount: '200',
-    payment_method: 'card',
-    payment_plan: '280',
+    payment_method: 'Cash',
+    payment_plan: 'Yearly',
     status: 'Paid Subscription'
   },
   {
     companyName: 'Aldar',
-    orderNo: '1100',
-    date: ' 12/30',
-    amount: '200',
-    payment_method: 'card',
-    payment_plan: '280',
+    orderNo: '2100',
+    date: '9-30-2023',
+    amount: '8200',
+    payment_method: 'Credit Card',
+    payment_plan: 'Yearly',
+    status: 'Paid Subscription'
+  },
+  {
+    companyName: 'Naruto',
+    orderNo: '1170',
+    date: '9-15-2023',
+    amount: '450',
+    payment_method: 'Cash',
+    payment_plan: 'Yearly',
+    status: 'Paid Subscription'
+  },
+  {
+    companyName: 'Naruto',
+    orderNo: '1190',
+    date: '12-15-2023',
+    amount: '2800',
+    payment_method: 'Visa Card',
+    payment_plan: 'Monthly',
     status: 'Paid Subscription'
   },
   {
     companyName: 'Aldar',
-    orderNo: '1100',
-    date: ' 12/30',
-    amount: '200',
-    payment_method: 'card',
-    payment_plan: '280',
-    status: 'Paid Subscription'
-  },
-  {
-    companyName: 'Aldar',
-    orderNo: '1100',
-    date: ' 12/30',
-    amount: '200',
-    payment_method: 'card',
-    payment_plan: '280',
-    status: 'Paid Subscription'
-  },
-  {
-    companyName: 'Aldar',
-    orderNo: '1100',
-    date: ' 12/30',
-    amount: '200',
-    payment_method: 'card',
-    payment_plan: '280',
-    status: 'Paid Subscription'
-  },
-  {
-    companyName: 'Aldar',
-    orderNo: '1100',
-    date: ' 12/30',
-    amount: '200',
-    payment_method: 'card',
-    payment_plan: '280',
-    status: 'Paid Subscription'
-  },
-  {
-    companyName: 'Aldar',
-    orderNo: '1100',
-    date: ' 12/30',
-    amount: '200',
-    payment_method: 'card',
-    payment_plan: '280',
-    status: 'Paid Subscription'
-  },
-  {
-    companyName: 'Aldar',
-    orderNo: '1100',
-    date: ' 12/30',
-    amount: '200',
-    payment_method: 'card',
-    payment_plan: '280',
-    status: 'Paid Subscription'
-  },
-  {
-    companyName: 'Aldar',
-    orderNo: '1100',
-    date: ' 12/30',
-    amount: '200',
-    payment_method: 'card',
-    payment_plan: '280',
-    status: 'Paid Subscription'
-  },
-  {
-    companyName: 'Aldar',
-    orderNo: '1100',
-    date: ' 12/30',
-    amount: '200',
-    payment_method: 'card',
-    payment_plan: '280',
-    status: 'Paid Subscription'
-  },
-  {
-    companyName: 'Aldar',
-    orderNo: '1100',
-    date: ' 12/30',
-    amount: '200',
-    payment_method: 'card',
-    payment_plan: '280',
-    status: 'Paid Subscription'
-  },
-  {
-    companyName: 'Aldar',
-    orderNo: '1100',
-    date: ' 12/30',
-    amount: '200',
-    payment_method: 'card',
-    payment_plan: '280',
-    status: 'Paid Subscription'
-  },
-  {
-    companyName: 'Aldar',
-    orderNo: '1100',
-    date: ' 12/30/2023',
+    orderNo: '1120',
+    date: '12-15-2023',
     amount: '212313',
-    payment_method: 'Card',
+    payment_method: 'Credit Card',
     payment_plan: 'Monthly',
     status: 'Paid Subscription'
   }
