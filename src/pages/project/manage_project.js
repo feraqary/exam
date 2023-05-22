f5975a87eaaf529e8b3c8b4898e1153a95bf9fe1
usman-finehome <133855254+usman-finehome@users.noreply.github.com>
2023-05-22 17:34:39 +0400

    local project datatable

--- src/pages/project/manage_project.js
+++ src/pages/project/manage_project.js
@@ -4,24 +4,23 @@ import { Grid } from '@mui/material';
 // project imports
 import Layout from 'layout';
 import Page from 'components/ui-component/Page';
-import MainCard from 'components/ui-component/cards/MainCard';
 import { gridSpacing } from 'store/constant';
+import LocalPro from 'components/widget/Data/local_pro_datatable';
 
-// ==============================|| Manage Local Project ||============================== //
-function ManageProject() {
-  return (
-    <Page title="Manage Project">
-      <Grid container spacing={gridSpacing}>
-        <Grid item xs={12}>
-          <MainCard title="Manage Local Project" />
-        </Grid>
+// ===========================|| International Company Managment list||=========================== //
+
+const IntCompData = () => (
+  <Page title="Local Project List">
+    <Grid container spacing={gridSpacing}>
+      <Grid item xs={12}>
+        <LocalPro />
       </Grid>
-    </Page>
-  );
-}
+    </Grid>
+  </Page>
+);
 
-ManageProject.getLayout = function getLayout(page) {
+IntCompData.getLayout = function getLayout(page) {
   return <Layout>{page}</Layout>;
 };
 
-export default ManageProject;
+export default IntCompData;
