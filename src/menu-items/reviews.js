e535f81176569f35aef634687fb86eb39f22eab6
Roushan Kashif <roushan@finehomeint.com>
2023-05-24 14:41:38 +0400

    added all pages under review section

--- src/menu-items/reviews.js
+++ src/menu-items/reviews.js
@@ -26,31 +26,29 @@ const reviews = {
       id: 'agent-reviews',
       title: <FormattedMessage id="Agent Reviews" />,
       type: 'item',
-      icon: icons.IconForms
+      icon: icons.IconForms,
+      url:'/review/agentreview'
     },
     {
       id: 'company-reviews',
       title: <FormattedMessage id="Company Reviews" />,
       type: 'item',
-      url: '/exchange/manage-units',
+      url:'/review/company',
       icon: icons.IconForms,
-      breadcrumbs: false
     },
     {
       id: 'project-reviews',
       title: <FormattedMessage id="Project Reviews" />,
       type: 'item',
-      url: '/exchange/manage-units',
+      url:'/review/project',
       icon: icons.IconForms,
-      breadcrumbs: false
     },
     {
       id: 'building-reviews',
       title: <FormattedMessage id="Building Reviews" />,
       type: 'item',
-      url: '/exchange/manage-units',
+      url:'/review/building',
       icon: icons.IconForms,
-      breadcrumbs: false
     }
   ]
 };
