5c1b77608d9713cb434f87ce9e5b6bebadee7571
Mohammad Usman <usman@finehomeint.com>
2023-05-27 15:19:20 +0400

    m.h: created reports-agents

--- src/pages/company/payments.js
+++ src/pages/company/payments.js
@@ -11,6 +11,47 @@ import Payment from 'components/widget/Data/company/payments';
 
 // ==============================|| Add Company Type form ||============================== //
 function Payments() {
+
+
+  const data = [
+    {
+      companyName: 'Aldar',
+      orderNo: '1100',
+      date: '3-05-2023',
+      amount: '7300',
+      payment_method: 'Visa Card',
+      payment_plan: 'Monthly',
+      status: 'Paid Subscription'
+    },
+    {
+      companyName: 'Aldar',
+      orderNo: '300',
+      date: '3-05-2023',
+      amount: '2000',
+      payment_method: 'Debit Card',
+      payment_plan: 'Monthly',
+      status: 'Paid Subscription'
+    },
+    {
+      companyName: 'Naruto',
+      orderNo: '1050',
+      date: '02-30-2023',
+      amount: '56000',
+      payment_method: 'Credit Card',
+      payment_plan: 'Monthly',
+      status: 'Paid Subscription'
+    },
+    {
+      companyName: 'Naruto',
+      orderNo: '1400',
+      date: '02-30-2023',
+      amount: '3120',
+      payment_method: 'Cash',
+      payment_plan: 'Yearly',
+      status: 'Paid Subscription'
+    },
+  ];
+  
   return (
     <Page title=" Pending Payments">
       <Grid container spacing={gridSpacing}>
