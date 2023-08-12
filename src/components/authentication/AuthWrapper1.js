35a35971ad5ff3962874c6bec2c3705329296bea
Mohamed Hassan Gebril <m.hassan@finehomeint.com>
2023-08-12 15:54:13 +0400

    login fixed

--- src/components/authentication/AuthWrapper1.js
+++ src/components/authentication/AuthWrapper1.js
@@ -1,11 +1,43 @@
 // material-ui
 import { styled } from '@mui/material/styles';
-
 // ==============================|| AUTHENTICATION 1 WRAPPER ||============================== //
 
 const AuthWrapper1 = styled('div')(({ theme }) => ({
-  backgroundColor: theme.palette.mode === 'dark' ? theme.palette.background.default : theme.palette.grey[100],
-  minHeight: '100vh'
+  position: 'relative', // Add position relative to the parent
+  '&::before': {
+    content: '""',
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    width: '100%',
+    height: '100%',
+    background: `url('/assets/bg1.jpg')`,
+    animation: 'zoom 7s ease-in-out infinite alternate',
+    backgroundPosition: 'center',
+    backgroundSize: 'cover',
+    backgroundRepeat: 'no-repeat'
+  },
+  '&::after': {
+    content: '""',
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    width: '100%',
+    height: '100%',
+    backgroundColor: 'rgba(0, 0, 0, 0.5)' // Gray overlay color
+  },
+  '@keyframe s zoom': {
+    from: {
+      transform: 'scale(1)'
+    },
+    to: {
+      transform: 'scale(1.02)'
+    }
+  },
+
+  // backgroundColor: theme.palette.mode === 'dark' ? theme.palette.background.default : theme.palette.grey[100],
+  minHeight: '100vh',
+  overflow: 'hidden'
 }));
 
 export default AuthWrapper1;
