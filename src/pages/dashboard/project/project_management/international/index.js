755574c9e218fcd18c26d17f27432fb7d059b9dd
Sulafa <sulafa@finehomeint.com>
2023-08-15 16:57:47 +0400

    block button layout fixed and add change promotion page ruote

--- src/pages/dashboard/project/project_management/international/index.js
+++ src/pages/dashboard/project/project_management/international/index.js
@@ -230,7 +230,7 @@ const international_Projects = () => {
                   </Button>
                 </Link>
 
-                <Button variant="contained" color="error">
+                <Button variant="outlined" color="error">
                   Block
                 </Button>
               </Box>
