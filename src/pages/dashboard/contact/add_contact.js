479cc33297373dba1486b2962aa16cd203ad2734
Hanya Zia <hanyan@finehomeint.com>
2023-08-23 14:56:58 +0400

    updated contacts

--- src/pages/dashboard/contact/add_contact.js
+++ src/pages/dashboard/contact/add_contact.js
@@ -1,8 +1,7 @@
-// material-ui
 import {
   Grid,
   RadioGroup,
-  CardActions,
+
   Chip,
   Box,
   FormControlLabel,
@@ -18,19 +17,14 @@ import {
 } from '@mui/material';
 import MuiPhoneNumber from 'material-ui-phone-number';
 import VerticalTabs from './contact_components/VerticalTabs';
-
 import InputLabel from 'components/ui-component/extended/Form/InputLabel';
 // project imports
 import Layout from 'layout';
 import Page from 'components/ui-component/Page';
-import MainCard from 'components/ui-component/cards/MainCard';
 import { gridSpacing } from 'store/constant';
-import AutocompleteForms from 'components/forms/forms-validation/AutocompleteForms';
 import { useState, useEffect } from 'react';
-import CustomDateTime from 'components/forms/components/DateTime/CustomDateTime';
 import Container from 'components/Elements/Container';
 import InputText from 'components/InputArea/TextInput';
-import FileUpload from 'components/InputArea/FileUpload';
 import Selector from 'components/InputArea/Selector';
 import AutoCompleteSelector from 'components/InputArea/AutoCompleteSelector';
 import InputDateTime from 'components/InputArea/InputDate';
@@ -38,9 +32,9 @@ import SubmitButton from 'components/Elements/SubmitButton';
 
 // ==============================|| Add Contact||============================== //
 function Add_contact() {
-  const Dummy = ['this', 'is', 'a', 'dummy'];
   const [managed, setManaged] = useState('');
   const [value, setvalue] = useState();
+  const [value1, setvalue1] = useState();
   const [Email, setEmail] = useState(true);
   const [personName, setPersonName] = useState([]);
   const [Active, setActive] = useState(false);
@@ -48,10 +42,9 @@ function Add_contact() {
     mobile: 0,
     email: 0
   });
-
+  const SUPPORTED_FORMATS = ['image/jpg', 'image/jpeg', 'image/png'];
   const [mobiles, setMobiles] = useState(0);
   const [emails, setEmails] = useState(0);
-
   const comp = [
     {
       tab: 'General info',
@@ -64,7 +57,6 @@ function Add_contact() {
       target: { value }
     } = event;
     setPersonName(
-      // On autofill we get a stringified value.
       typeof value === 'string' ? value.split(',') : value
     );
   };
@@ -113,82 +105,119 @@ function Add_contact() {
 
             <Grid item xs={12} lg={6}>
               <Selector
-                label="Company Type"
-                id="salutation-selector"
+                label="Contact Type"
+                name="contact_type"
+                id="contact_type"
                 options={['Individual', 'Company']}
                 style={{ xs: 12, lg: 6 }}
                 placeholder="Select Salutation"
+                {...getFieldProps('salutation')}
               />
             </Grid>
             <Grid item xs={12} lg={6}></Grid>
-
             <AutoCompleteSelector
               label="Salutation"
               style={{ xs: 12, lg: 6 }}
               options={['Mr.', 'Mrs.']}
-              placeholder="select image"
+              placeholder="Select Reference ID Salutation"
+              id="salutation"
+              name="salutation"
               fullWidth
             />
 
             <InputText
               label="Name"
-              helperText="Pleas enter first name"
-              placeholder={'enter first name...'}
+              helperText="Enter Name"
+              placeholder={'Enter Name...'}
+              id="name"
+              name="name"
               style={{ xs: 12, lg: 6 }}
               type="text"
               value={value}
               setValue={setvalue}
             />
-
             <InputText
-              label="Family Name"
-              helperText="Pleas enter family name"
-              placeholder={'enter family name...'}
+              label="Enter Family Name"
+              helperText="family name"
+              placeholder={'Enter Family Name...'}
+              name=" family_name"
+              id="family_name"
               style={{ xs: 12, lg: 6 }}
               type="text"
               value={value}
               setValue={setvalue}
             />
-
             <AutoCompleteSelector
               label="Language"
               style={{ xs: 12, lg: 6 }}
               options={['Arabic', 'English']}
-              placeholder={'select language...'}
-              helperText={'please choose a language...'}
-              id="add-contact-language"
+              placeholder={'Select Language...'}
+              id="contact_language"
+              name="contact_language"
               fullWidth
             />
 
             <Grid item xs={12} lg={6}>
               <InputLabel required>Mobile </InputLabel>
               <MuiPhoneNumber fullWidth variant="outlined" defaultCountry={'ae'} />
-              <FormHelperText>please enter mobile number</FormHelperText>
+              <FormHelperText>Please Enter Primary Mobile Number</FormHelperText>
             </Grid>
-
             <Grid item xs={12} lg={6}>
-              <InputLabel>Landline </InputLabel>
+              <InputLabel required>Mobile 2 </InputLabel>
               <MuiPhoneNumber fullWidth variant="outlined" defaultCountry={'ae'} />
-              <FormHelperText>please enter landline number</FormHelperText>
+              <FormHelperText>Please Enter Secondary Mobile Number</FormHelperText>
             </Grid>
-
-            <AutoCompleteSelector
-              label="Contact preference"
-              style={{ xs: 12, lg: 6 }}
-              options={['Mobile', 'Landline', 'Email']}
-              id="add-contact-contact-preference"
-              fullWidth
-              placeholder={'select contact preference...'}
-              helperText={'please choose a contact preference'}
-            />
-
+            <Grid item xs={12} lg={6}>
+              <InputLabel>Landline Phone</InputLabel>
+              <MuiPhoneNumber fullWidth variant="outlined" defaultCountry={'ae'} />
+              <FormHelperText>please enter Landline number</FormHelperText>
+            </Grid>
             <Grid item xs={12} lg={6}>
               <InputLabel>Fax</InputLabel>
               <MuiPhoneNumber fullWidth variant="outlined" defaultCountry={'ae'} />
-              <FormHelperText>please enter fax</FormHelperText>
+              <FormHelperText>Enter Fax Number</FormHelperText>
             </Grid>
+            {Email && (
+              <>
+                <InputText
+                  label="Email"
+                  helperText="Please Enter Primary Contact Email Address"
+                  style={{ xs: 12, lg: 6 }}
+                  type="email"
+                  name="primary"
+                  id="primary_email"
+                  value={value1}
+                  setValue={setvalue1}
+                  placeholder={'Enter Email Address...'}
+                />
+              </>
+            )}
+               {Email && (
+              <>
+                <InputText
+                  label="Secondary Email Address"
+                  helperText="Please Enter Secondary Contact Email Address"
+                  style={{ xs: 12, lg: 6 }}
+                  type="email"
+                  name="secondary"
+                  id="secondary_email"
+                  value={value}
+                  setValue={setvalue}
+                  placeholder={'Enter Email Address...'}
+                />
+              </>
+            )}  
+            <AutoCompleteSelector
+              label="Contact Preference"
+              style={{ xs: 12, lg: 6 }}
+              options={['Primary Mobile Number','Secondary Mobile Number', 'Landline', 'Email', 'Both Email and Phone']}
+              id="contact_preferences"
+              name="contact_preferences"
+              fullWidth
+              placeholder={'Select the Contract Preferences..'}
+              helperText={'Please choose a Contact Preference'}
+            />
             <Grid item xs={12} lg={6}>
-              {/* <FormGroup row> */}
               <FormControlLabel
                 control={<Checkbox value={'i'} />}
                 label="Client not willing to give Email address"
@@ -196,21 +225,47 @@ function Add_contact() {
                   setEmail(!Email);
                 }}
               />
-              {/* </FormGroup> */}
+            </Grid>
+            <Grid item xs={12} lg={6}>
+              <InputLabel>Managed</InputLabel>
+              <RadioGroup
+                aria-labelledby="radio-buttons-group-label"
+                row
+                onChange={(e) => {
+                  setManaged(e.target.value);
+                }}
+              >
+                <FormControlLabel value="yes" control={<Radio />} label="Yes" />
+                <FormControlLabel value="no" control={<Radio />} label="No" />
+              </RadioGroup>
             </Grid>
 
-            {Email && (
+            {managed === 'yes' ? (
               <>
-                <InputText
-                  label="Email"
-                  helperText="Pleas enter email address"
+                <AutoCompleteSelector
+                  label="Relationship Manager"
                   style={{ xs: 12, lg: 6 }}
-                  type="text"
-                  value={value}
-                  setValue={setvalue}
-                  placeholder={'enter email address...'}
+                  options={Dummy}
+                  id="relationship_manager"
+                  name="relationship_manager"
+                  fullWidth
+                  helperText="Please Enter Relationship Manager"
+                  placeholder={'relationship manager...'}
+                />
+
+                <AutoCompleteSelector
+                  label="Managed Category"
+                  style={{ xs: 12, lg: 6 }}
+                  options={Dummy}
+                  id="managed_category"
+                  name="managed_category"
+                  fullWidth
+                  helperText="please enter manager category...."
+                  placeholder={'manager category...'}
                 />
               </>
+            ) : (
+              <></>
             )}
 
             <InputText
@@ -270,47 +325,13 @@ function Add_contact() {
                   }} 
                   id="salutation-selector" options={["yes", "no"]} style={{ xs: 12, lg: 6 }} placeholder="Select Salutation" /> */}
 
-            <Grid item xs={12} lg={6}>
-              <InputLabel>Managed</InputLabel>
-              <RadioGroup
-                aria-labelledby="radio-buttons-group-label"
-                row
-                onChange={(e) => {
-                  setManaged(e.target.value);
-                }}
-              >
-                <FormControlLabel value="yes" control={<Radio />} label="Yes" />
-                <FormControlLabel value="no" control={<Radio />} label="No" />
-              </RadioGroup>
-            </Grid>
-
-            {managed === 'yes' ? (
-              <>
-                <AutoCompleteSelector
-                  label="Relationship Manager"
-                  style={{ xs: 12, lg: 6 }}
-                  options={Dummy}
-                  fullWidth
-                  helperText="please enter relationship manager...."
-                  placeholder={'relationship manager...'}
-                />
-
-                <AutoCompleteSelector
-                  label="Managed Category"
-                  style={{ xs: 12, lg: 6 }}
-                  options={Dummy}
-                  fullWidth
-                  helperText="please enter manager category...."
-                  placeholder={'manager category...'}
-                />
-              </>
-            ) : (
-              <></>
-            )}
+         
             <AutoCompleteSelector label="Shared With" style={{ xs: 12, lg: 6 }} options={names} fullWidth />
             <InputText
               label="Ejari Contact Ref. No."
               helperText="Pleas enter ejari contact ref. no."
+              name="ejari_number"
+              id="ejari_number"
               placeholder="ref. no. ..."
               style={{ xs: 12, lg: 6 }}
               type="text"
