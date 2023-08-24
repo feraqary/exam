ca3fe518a334dc1755879be0bb318e39b2975375
Ray <r.rafique@finehomeint.com>
2023-08-24 17:25:22 +0400

    English translation in JS for menuItems

---
+++ src/utils/locales/en.js
@@ -0,0 +1,196 @@
+export default {
+  en: {
+    dashboard: {
+      menuItems: {
+        agents: {
+            addAgent: 'Add agent',
+            manageLocalAgents: 'Manage Local Agents',
+            manageInternationalAgents: 'Manage International Agents',
+            manageDeletedAgents: 'Manage Deleted Agents',
+            activities: 'Activities'
+        },
+        analytics: {
+            statistics: 'Statistics'
+        },
+        app: {
+            posts: 'Posts',
+            documents: 'Documents',
+            manageDocuments: 'Manage Documents'
+        },
+        auctions: {
+            auctionUsers: 'Auction Users',
+            manageLocalAuction: 'Manage Local Auction',
+            manageInternationalAuction: 'Manage International Auction',
+            managePartners: 'Manage Partners',
+            manageDeletedAuctions: 'Manage Deleted Auctions',
+            activities: 'Activities',
+            faqs: 'FAQs'
+        },
+        bannerAds: {
+            addBanner: 'Add Banner',
+            manageBanners: 'Manage Banners',
+            manageCompanyVideos: 'Manage Company Videos',
+            manageProjectVideos: 'Manage Project Videos',
+            manageTowerVideos: 'Manage Tower Videos',
+            manageDeletedBanners: 'Manage Deleted Banners',
+            activities: 'activities'
+        },
+        blogs: {
+            addBlog: 'Add Blog',
+            manageBlogs: 'Manage Blogs',
+            categories: 'Categories',
+            deletedBlogs: 'Deleted Blogs',
+            activities: 'Activities'
+        },
+        careers: {
+            jobs: 'Jobs',
+            categories: 'Categories',
+            articles: 'Articles',
+            deletedCareers: 'Deleted Careers',
+            activities: 'Activities'
+        },
+        companies: {
+          addCompany: 'Add Company',
+          addSubCompany: 'Add Sub Company',
+          manageSubCompany: 'Manage Sub Company',
+          localCompanies: 'Local Companies',
+          internationalCompanies: 'International Companies',
+          featuredCompanies: 'Featured Companies',
+          blockedCompanies: 'Blocked Companies',
+          addSubscriptions: 'Add Subscriptions',
+          viewActive: 'View Active',
+          viewPending: 'View Pending',
+          payments: 'Payments',
+          companyPrivileges: 'Company Privileges',
+          activities: 'Activities'
+        },
+        communityGuide: {
+            community: 'Community',
+            towerImage: 'Tower Image',
+            changeCommunityPublishStatus: 'Change Community Publish Status',
+            changeTowerStatus: 'Change Tower Status',
+            activities: 'activities'
+        },
+        contact: {
+            addContact: 'Add Contact',
+            manageContacts: 'Manage Contacts'
+        },
+        contracts: {
+            addContract: 'Add Contract',
+            manageContracts: 'Manage Contracts'
+        },
+        exchange: {
+            localUnits: 'Local Units',
+            internationalUnits: 'International Units',
+            offerUnits: 'Offer Units',
+            manageOfferCategories: 'Manage Offer Categories',
+            activities: 'Activities'
+        },
+        exhibitions: {
+            addExhibitions: 'Add Exhibitions',
+            manageLocalExhibitions: 'Manage Local Exhibitions',
+            manageInternationalExhibitions: 'Manage International Exhibitions',
+            manageClients: 'Manage Clients',
+            manageExhibitionServices: 'Manage Exhibition Services',
+            activities: 'Activities'
+        },
+        leads: {
+            addLeads: 'Add Leads',
+            manageLeads: 'Manage Leads',
+            videoRequests: 'Video Requests',
+            generalRequests: 'General Requests',
+            viewSchedules: 'View Schedules'
+        },
+        management: {
+            addUser: 'Add User',
+            manageUsers: 'Manage Users',
+            manageUserRoles: 'Manage User Roles',
+            manageDepartment: 'Manage Department',
+            manageDeletedUsers: 'Manage Deleted Users',
+            activities: 'Activities'
+        },
+        pages: {
+            addPages: 'Add Pages',
+            managePages: 'Manage Pages',
+            manageAdverts: 'Manage Adverts',
+            manageExchangeContents: 'Manage Exchange Contents',
+            manageExhibitionContents: 'Manage Exhibition Contents',
+            manageTowerContents: 'Manage Tower Contents',
+            manageAuctionContents: 'Manage Auction Contents'
+        },
+        project: {
+            addProject: 'Add Project',
+            localProjects: 'Local Projects',
+            internationalProjects: 'International Projects',
+            createCategory: 'Create Category',
+            sharedProjects: 'Shared Projects',
+            blockedProjects: 'Blocked Projects',
+            deletedProjects: 'Deleted Projects',
+            managePromotions: 'Manage Promotions',
+            manageRequests: 'Manage Requests',
+            activities: 'Activities'
+        },
+        propertyHub: {
+            addProperty: 'Add Property',
+            localProperties: 'Local Properties',
+            internationalProperties: 'International Properties',
+            sharedProperties: 'Shared Properties',
+            deletedProperties: 'Deleted Properties',
+            activities: 'Activities'
+        },
+        reports: {
+            location: 'Location',
+            competitive: 'Competitive',
+            agents: 'Agents',
+            transactions: 'Transactions',
+            market: 'Market'
+        },
+        reviews: {
+            agentReviews: 'Agent Reviews',
+            companyReviews: 'Company Reviews',
+            projectReviews: 'Project Reviews',
+            buildingReviews: 'Building Reviews'
+        },
+        services: {
+            addMainServices: 'Add Main Services',
+            manageMainServices: 'Manage Main Services',
+            addSubServices: 'Add Sub Services',
+            manageSubServices: 'Manage Sub Services',
+            serviceActivities: 'Service Activities'
+        },
+        settings: {
+            xmlFeeds: 'XML Feeds',
+            amenities: 'Amenities',
+            facilities: 'Facilities',
+            imageCategories: 'Image Categories',
+            internationalContents: 'International Contents',
+            vatTax: 'VAT/TAX',
+            subscriptions: 'Subscriptions',
+            subscriptionCost: 'Subscription Cost',
+            menu: 'Menu',
+            menuCountryWise: 'Menu Country Wise',
+            activities: 'Activities'
+        },
+        subscibers: {
+            subscribersList: 'Subscribers List'
+        },
+        units: {
+            addUnit: 'Add Unit',
+            saleUnits: 'Sale Units',
+            rentUnits: 'Rent Units',
+            sharedUnits: 'Shared Units',
+            internationalUnits: 'International Units',
+            rejectedUnits: 'Rejected Units',
+            deletedUnits: 'Deleted Units',
+            activities: 'Activities',
+            problemReports: 'Problem Reports'
+        },
+        users: {
+            normalUsers: 'Normal Users',
+            registeredUsers: 'Registered Users',
+            pendingUsers: 'Pending Users'
+        }
+      }
+    }
+  }
+};
